5a92d17d9bad644521840f946e7b58703d819cb2
Łukasz Kowalewski <kowalewski.ukasz@gmail.com>
2022-10-31 19:11:21 +0100

    Login screen added.

--- app/src/main/java/com/example/applause/MainActivity.java
+++ app/src/main/java/com/example/applause/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.applause;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 
     @Override
