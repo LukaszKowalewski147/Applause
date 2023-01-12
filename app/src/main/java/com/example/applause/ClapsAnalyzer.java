17d142bde59ff757d16d026ee5f8ee4d645e9a2d
Lukasz Kiepas <reignover1309@gmail.com>
2023-01-12 20:24:35 +0100

    properly working quality algorithm implented into android app

--- app/src/main/java/com/example/applause/ClapsAnalyzer.java
+++ app/src/main/java/com/example/applause/ClapsAnalyzer.java
@@ -138,12 +138,77 @@ public class ClapsAnalyzer {
     }
 
     private int calculateQuality() {
-        for(Clap clap : claps){
+        int i = 0;
+        Double sumaCzasuKlasniec = 0.0;
+        Double sumaMocyKlasniec =  0.0;
 
+        // srednia algebraiczna czasu na klasniecie i mocy.
+        for(Clap clap : claps){
+            i++;
+            sumaCzasuKlasniec = sumaCzasuKlasniec + getTimeInSeconds(clap.getTime());
+            sumaMocyKlasniec = sumaMocyKlasniec +  clap.getzAcceleration();
+        }
+        System.out.println("SUMA: " + sumaCzasuKlasniec + " sekund");
+        System.out.println("SUMA: " + sumaMocyKlasniec + " moc");
+        Double sredniaSumaCzasuKlasniec = sumaCzasuKlasniec / i;
+        Double sredniaSumaMocyKlasniec = sumaMocyKlasniec / i;
+
+
+
+
+// jakosc czasu !!!!
+        LinkedList<Double> roznicaProcentowaCzasu = new LinkedList<>();
+        for(Clap clap : claps){
+            Double klasniecieCzas = getTimeInSeconds(clap.getTime());
+
+            if(klasniecieCzas == sredniaSumaCzasuKlasniec ){
+                roznicaProcentowaCzasu.add(1.0);
+            } else if (klasniecieCzas > sredniaSumaCzasuKlasniec) {
+                roznicaProcentowaCzasu.add((klasniecieCzas - sredniaSumaCzasuKlasniec) / klasniecieCzas * 100);
+            }else {
+                roznicaProcentowaCzasu.add((sredniaSumaCzasuKlasniec - klasniecieCzas) / sredniaSumaCzasuKlasniec * 100);
+            }
         }
 
-        int quality = 0;
-        return quality;
+        double jakoscCzasu = 0;
+        for(int x =0; x < roznicaProcentowaCzasu.size(); x++){
+            jakoscCzasu = jakoscCzasu + roznicaProcentowaCzasu.get(x);
+            System.out.println("ROZNICA CZASU % " + roznicaProcentowaCzasu.get(x));
+        }
+        jakoscCzasu = 100 - (jakoscCzasu   / roznicaProcentowaCzasu.size());
+        System.out.println("JAKOSC CZASU % " + (jakoscCzasu));
+
+
+        // jakosc sily  !!!!
+        LinkedList<Double> roznicaProcentowaSily = new LinkedList<>();
+        for(Clap clap : claps){
+            Double silaMoc = clap.getzAcceleration();
+
+            if(silaMoc == sredniaSumaMocyKlasniec ){
+                roznicaProcentowaSily.add(1.0);
+            } else if (silaMoc > sredniaSumaMocyKlasniec) {
+                roznicaProcentowaSily.add((silaMoc - sredniaSumaMocyKlasniec) / silaMoc * 100);
+            }else {
+                roznicaProcentowaSily.add((sredniaSumaMocyKlasniec - silaMoc) / sredniaSumaMocyKlasniec * 100);
+            }
+        }
+
+        double jakoscMocy = 0;
+        for(int x =0; x < roznicaProcentowaSily.size(); x++){
+            jakoscMocy = jakoscMocy + roznicaProcentowaSily.get(x);
+            System.out.println("ROZNICA MOCY % " + roznicaProcentowaSily.get(x));
+        }
+        jakoscMocy = 100 - (jakoscMocy   / roznicaProcentowaSily.size()) ;
+        System.out.println("JAKOSC MOCY % " + (jakoscMocy));
+
+
+        // ogolna jakosc ((sila  + czas) / 2))
+        double ogolnaJakosc = (jakoscCzasu + jakoscMocy) / 2;
+        System.out.println("OGOLA JAKOSC: " + ogolnaJakosc);
+
+
+
+        return (int)ogolnaJakosc;
     }
 
     private int calculateReflex() {
